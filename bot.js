a15a4d3cfa0a0a808c1b8227c22744796bab74e9
PiVortex <owengeorgehassall@gmail.com>
2025-03-04 11:05:22 +0000

    initial commit

---
+++ bot.js
@@ -0,0 +1,49 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+const grammy_1 = require("grammy");
+const dotenv = __importStar(require("dotenv"));
+dotenv.config();
+// Get the bot token from environment variables
+const token = process.env.BOT_TOKEN;
+if (!token) {
+    throw new Error("BOT_TOKEN environment variable is not set");
+}
+const bot = new grammy_1.Bot(token);
+// Handle the /start command.
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+// Handle other messages.
+bot.on("message", (ctx) => ctx.reply("Got another message!"));
+bot.start();
---
+++ bot.ts
@@ -0,0 +1,22 @@
+import { Bot } from "grammy";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+// Get the bot token from environment variables
+const token = process.env.BOT_TOKEN;
+if (!token) {
+  throw new Error("BOT_TOKEN environment variable is not set");
+}
+
+const bot = new Bot(token);
+
+
+// Handle the /start command.
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+
+// Handle other messages.
+bot.on("message", (ctx) => ctx.reply("Got another message!"));
+
+
+bot.start();
